12b8848e9c37f8cd458addbdb02403276ec60393
thatsn0tmysite <17581197+thatsn0tmysite@users.noreply.github.com>
2024-06-14 01:49:58 +0200

    corrected version

--- tget/tget.go
+++ tget/tget.go
@@ -20,7 +20,7 @@ var TorrcTemplate string
 type TorGet struct {
 }
 
-var Version = "v0.3a"
+var Version = "v0.3.1"
 
 func PrepareRequest(req *http.Request, headers []string, cookies, useragent, body string) {
 	for _, h := range headers {
